5f702a5aa4829974e1c20aeb9a2d0fea25c0b535
James Clarke <jamesraymondclarke@gmail.com>
2017-07-01 17:54:15 +0100

    Authentication and secureRoute working fully, might be buggy

---
+++ src/js/controllers/main.js
@@ -0,0 +1,31 @@
+angular
+.module('groupProject')
+.controller('MainCtrl', MainCtrl);
+
+MainCtrl.$inject = ['$rootScope','$state','$auth'];
+function MainCtrl($rootScope, $state, $auth) {
+  const vm = this;
+
+  vm.isAuthenticated = $auth.isAuthenticated;
+
+  $rootScope.$on('error', (e, err) => {
+    vm.message = err.data.message;
+
+    if(err.status === 401) {
+      vm.stateHasChanged = false;
+      $state.go('login');
+    }
+  });
+
+  $rootScope.$on('$stateChangeSuccess', (e, state) => {
+    vm.pageName = state.name;
+    if(vm.stateHasChanged) vm.message = null;
+    if(!vm.stateHasChanged) vm.stateHasChanged = true;
+  });
+
+  function logout() {
+    $auth.logout();
+    $state.go('home');
+  }
+  vm.logout = logout;
+}
