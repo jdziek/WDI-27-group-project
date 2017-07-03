f78a0cf652532b40ba02c44a9dd78981e6828c0b
Simon Amor <simonrramor@gmail.com>
2017-07-03 12:22:20 +0100

    Main and User ctrl changed

--- src/js/controllers/user.js
+++ src/js/controllers/user.js
@@ -2,13 +2,11 @@ angular
   .module('groupProject')
   .controller('ProfileCtrl', ProfileCtrl);
 
-ProfileCtrl.$inject = ['$auth', 'User', '$state'];
-function ProfileCtrl($auth, User, $state) {
+ProfileCtrl.$inject = ['$auth', 'User', '$state', 'UserComment'];
+function ProfileCtrl($auth, User, $state, UserComment) {
   const vm = this;
 
-  const { userId } = $auth.getPayload();
-
-  if(userId) vm.user = User.get({ id: userId });
+  vm.user = User.get($state.params);
 
   function logout() {
     $auth.logout();
@@ -16,4 +14,28 @@ function ProfileCtrl($auth, User, $state) {
   }
 
   vm.logout = logout;
+
+  vm.newComment = {};
+
+  function addComment() {
+    UserComment
+    .save({ userId: vm.user.id }, vm.newComment)
+    .$promise
+    .then((comment) => {
+      vm.user.comments.push(comment);
+      vm.newComment = {};
+    });
+  }
+  vm.addComment = addComment;
+
+  function deleteComment(comment) {
+    UserComment
+      .delete({ userId: vm.user.id, id: comment.id })
+      .$promise
+      .then(() => {
+        const index = vm.user.comments.indexOf(comment);
+        vm.user.comments.splice(index, 1);
+      });
+  }
+  vm.deleteComment = deleteComment;
 }
