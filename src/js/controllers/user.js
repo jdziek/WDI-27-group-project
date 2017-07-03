413040b3191c0a7266c1d2ab8e0c0a4bd2018d60
Simon Amor <simonrramor@gmail.com>
2017-07-03 10:10:04 +0100

    user comments added

--- src/js/controllers/user.js
+++ src/js/controllers/user.js
@@ -2,8 +2,8 @@ angular
   .module('groupProject')
   .controller('ProfileCtrl', ProfileCtrl);
 
-ProfileCtrl.$inject = ['$auth', 'User', '$state'];
-function ProfileCtrl($auth, User, $state) {
+ProfileCtrl.$inject = ['$auth', 'User', '$state', 'UserComment', '$stateParams'];
+function ProfileCtrl($auth, User, $state, UserComment) {
   const vm = this;
 
   const { userId } = $auth.getPayload();
@@ -16,4 +16,28 @@ function ProfileCtrl($auth, User, $state) {
   }
 
   vm.logout = logout;
+
+  vm.newComment = {};
+
+  function addComment() {
+    UserComment
+    .save({ userId: vm.user.id }, vm.newComment)
+    .$promise
+    .then((comment) => {
+      vm.user.comments.push(comment);
+      vm.newComment = {};
+    });
+  }
+  vm.addComment = addComment;
+
+  function deleteComment(comment) {
+    UserComment
+      .delete({ userId: vm.user.id, id: comment.id })
+      .$promise
+      .then(() => {
+        const index = vm.user.comments.indexOf(comment);
+        vm.user.comments.splice(index, 1);
+      });
+  }
+  vm.deleteComment = deleteComment;
 }
