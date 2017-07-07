9489cc8b5bc248dbc704c8c195d04f91f38a12dc
Jakub Dziekan <jdziek89@gmail.com>
2017-07-07 11:47:46 +0100

    Adding redirect url

--- config/oauth.js
+++ config/oauth.js
@@ -4,7 +4,7 @@ const facebook = {
   accessTokenUrl: 'https://graph.facebook.com/v2.9/oauth/access_token',
   clientId: process.env.FACEBOOK_APP_ID,
   clientSecret: process.env.FACEBOOK_APP_SECRET,
-  redirectUri: 'http://localhost:7000/',
+  redirectUri: process.env.NODE_ENV === 'production'? 'https://our-awesome-app.herokuapp.com/' : 'http://localhost:7000/',
   responseCode: 'code'
 };
 module.exports = {
